175edd79f3984ba5be843b68efa80bff3d4f58e6
semoheva123 <semoheva@gmail.com>
2026-08-27 20:00:08 +0300

    Update Firebase configuration and add analytics

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,6 @@
 // firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js";
 import { 
   getFirestore, collection, addDoc, deleteDoc, doc, updateDoc, onSnapshot, getDoc, setDoc 
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
@@ -8,19 +9,21 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "efrin-one-store.firebaseapp.com",
-  projectId: "efrin-one-store",
-  storageBucket: "efrin-one-store.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBObnUf_n6dcpQEiNkE0Wcn8et_Fmn9jDY",
+  authDomain: "store-afbeb.firebaseapp.com",
+  projectId: "store-afbeb",
+  storageBucket: "store-afbeb.firebasestorage.app",
+  messagingSenderId: "727224850533",
+  appId: "1:727224850533:web:1b9c776e402bfc1d474e8c",
+  measurementId: "G-KVNDNVWYK3"
 };
 
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const db = getFirestore(app);
 const auth = getAuth(app);
 
 export { 
-  db, auth, collection, addDoc, deleteDoc, doc, updateDoc, onSnapshot, getDoc, setDoc,
+  db, auth, analytics, collection, addDoc, deleteDoc, doc, updateDoc, onSnapshot, getDoc, setDoc,
   createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged 
 };
